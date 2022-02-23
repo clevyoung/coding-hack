10ff0ce988c4f33ffa0a43dec6b9c5b7540ba90b
clevyoung <clevyoung@gmail.com>
2022-02-23 22:40:22 +0900

    정렬 관련해서 typed array, 원본 배열을 수정하지 않고 복사 배열을 만들어서 정렬하는 방법 추가

--- 정렬/basic.js
+++ 정렬/basic.js
@@ -4,3 +4,12 @@ const numbers = [15, 52, 23, 11, 9];
 numbers.sort((a, b) => a - b);
 // 내림차순 정렬, 원본 배열이 다시 수정
 numbers.sort((a, b) => b - a);
+
+// 형식화 배열(Typed Array)
+const copyNumbers = new Int32Array(numbers);
+
+copyNumbers.sort();
+
+// 원본 배열을 수정하기 않고 정렬
+const ascSortedNumbers = [...numbers].sort((a, b) => a - b);
+const descSortedNumbers = [...numbers].sort((a, b) => b - a);
